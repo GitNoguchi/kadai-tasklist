e83444072ec40ef3ac41f6d2817e702de0eec8bf
Noguchi <akatuakinoguti1022@outlook.jp>
2019-04-21 11:02:55 +0000

    add column validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,6 +7,12 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.index']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
         
                 <div class="form-group">
                     {!! Form::label('content', '作成ページ:') !!}
